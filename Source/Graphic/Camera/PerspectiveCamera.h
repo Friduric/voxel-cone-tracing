7ac9c6c8026e09403ccd7615454ef6b470bf2406
prantare@gmail.com <prantare@gmail.com>
2016-12-18 14:09:14 +0100

    Adjusted default FOV. Signed-off-by: prantare@gmail.com <prantare@gmail.com>

--- Source/Graphic/Camera/PerspectiveCamera.h
+++ Source/Graphic/Camera/PerspectiveCamera.h
@@ -5,5 +5,5 @@
 /// <summary> A perspective camera. </summary>
 class PerspectiveCamera : public Camera {
 public:
-	PerspectiveCamera(float _fov = 1.2, float _aspect = 1.0, float _near = 0.1, float _far = 500);
+	PerspectiveCamera(float _fov = 0.7, float _aspect = 1.0, float _near = 0.1, float _far = 500);
 };
